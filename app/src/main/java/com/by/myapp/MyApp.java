23b4de96395b59641e108e93c28fd5c22341ed02
yangtianfu <2812420513@qq.com>
2023-07-27 17:58:48 +0800

    init

---
+++ app/src/main/java/com/by/myapp/MyApp.java
@@ -0,0 +1,15 @@
+package com.by.myapp;
+
+import android.app.Application;
+
+/**
+ * @Author yangtianfu
+ * @Date 2023/7/27 14:29
+ * @Describe
+ */
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
