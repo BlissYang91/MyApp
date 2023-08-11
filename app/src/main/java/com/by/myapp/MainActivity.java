de106ecc830ab453aded615c9d2e34025a4431ba
yangtianfu <2812420513@qq.com>
2023-08-11 14:38:50 +0800

    AccessService

--- app/src/main/java/com/by/myapp/MainActivity.java
+++ app/src/main/java/com/by/myapp/MainActivity.java
@@ -2,13 +2,27 @@ package com.by.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.by.myapp.service.AccessService;
+import com.by.myapp.service.BaseService;
 
+public class MainActivity extends AppCompatActivity {
+    private  BaseService instance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        instance = BaseService.getInstance();
+        instance.init(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!instance.checkAccessibilityEnabled(getResources().getString(R.string.access_name))) {
+            instance.goAccess();
+        }
     }
 }
